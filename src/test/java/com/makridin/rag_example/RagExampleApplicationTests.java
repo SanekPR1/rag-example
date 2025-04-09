63e0ef45b1b72d907e6542f32d92a3cf16b06b94
Aleksandr Makridin <epam_amakridin@exactsciences.com>
2025-04-08 20:02:45 -0500

    #0000: initial commit

---
+++ src/test/java/com/makridin/rag_example/RagExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makridin.rag_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
